ae6012fdc18658de7436d0023c6f27b2a3044d17
Han-SY-97 <tpdud2204@gmail.com>
2024-08-02 16:42:15 +0900

    voicebot chatting ui 구현

--- voicebot_stt_tts.py
+++ voicebot_stt_tts.py
@@ -120,6 +120,16 @@ def main():
             now = datetime.now().strftime("%H:%M")
             st.session_state["chat"] = st.session_state["chat"] + [("bot", now, reponse)]
 
+            # 채팅 형식으로 시각화 하기
+            for sender, time, message in st.session_state["chat"]:
+                if sender == "user":
+                    st.write(f'<div style="display:flex;align-items:center;"><div style="background-color:#007AFF;color:white;border-radius:12px;padding:8px 12px;margin-right:8px;">{message}</div><div style="font-size:0.8rem;color:gray;">{time}</div></div>', 
+                             unsafe_allow_html=True)
+                    st.write("")
+                else:
+                    st.write(f'<div style="display:flex;align-items:center;justify-content:flex-end;"><div style="background-color:lightgray;border-radius:12px;padding:8px 12px;margin-left:8px;">{message}</div><div style="font-size:0.8rem;color:gray;">{time}</div></div>', 
+                             unsafe_allow_html=True)
+                    st.write("")
         else:
             st.session_state["check_reset"] = False
 
