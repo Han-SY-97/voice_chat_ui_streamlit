5091b00ec1feeada4cd98e188dfe4a02c3d8718e
Han-SY-97 <tpdud2204@gmail.com>
2024-08-02 16:20:36 +0900

    voicebot  STT 구현

--- voicebot_stt_tts.py
+++ voicebot_stt_tts.py
@@ -7,9 +7,35 @@ from dotenv import load_dotenv
 # .env 파일 경로 지정
 load_dotenv()
 
+# audiorecorder 패키지 추가
+from audiorecorder import audiorecorder
+# 시간 정보를 위한 패키지 추가
+from datetime import datetime
+
 # API 키 설정
 api_key = os.environ.get('OPEN_API_KEY')
 client = openai.OpenAI(api_key=api_key)
+
+# 기능 구현 함수
+def STT(speech):
+    # 파일 저장
+    filename = 'input.mp3'
+    speech.export(filename, format="mp3")
+
+    # 음원 파일 열기
+    with open(filename, "rb") as audio_file:
+        # Whisper 모델을 활용해 텍스트 얻기
+        transcription = client.audio.transcriptions.create(
+            model="whisper-1",
+            file=audio_file
+        )
+        
+    # 파일 삭제
+    os.remove(filename)
+
+    return transcription.text
+
+
 
 # 메인 함수
 def main():
@@ -65,6 +91,19 @@ def main():
     col1, col2 = st.columns(2)
     with col1:
         st.subheader("질문하기")
+        # 음성 녹음 아이콘 추가
+        audio = audiorecorder()
+        if (audio.duration_seconds > 0) and (st.session_state["check_reset"]==False):
+            # 음성 재생
+            st.audio(audio.export().read())
+            # 음원 파일에서 텍스트 추출
+            question = STT(audio)
+            # 채팅을 시각화하기 위해 질문 내용 저장
+            now = datetime.now().strftime("%H:%M")
+            st.session_state["chat"] = st.session_state["chat"] + [("user", now, question)]
+            # GPT 모델에 넣을 프롬프트를 위해 질문 내용 저장
+            st.session_state["messages"] = st.session_state["messages"] + [{"role": "user", "content": question}]
+
     with col2:
         st.subheader("질문/답변")
 
